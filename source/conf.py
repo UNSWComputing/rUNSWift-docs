575919e0844c45402d6d17f60a285b0253b0c0b6
ijnek <kenjibrameld@gmail.com>
2022-12-15 20:22:55 +0900

    add sphinx and sphinx-copybutton as dependency
    Signed-off-by: ijnek <kenjibrameld@gmail.com>

--- source/conf.py
+++ source/conf.py
@@ -29,7 +29,10 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.githubpages']
+extensions = [
+    'sphinx_copybutton'
+    'sphinx.ext.githubpages',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -64,7 +67,7 @@ release = '2020'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
